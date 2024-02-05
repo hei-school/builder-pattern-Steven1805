79407ab62c37a07cf70c052a50c0e17864c8bb8a
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2024-02-05 19:02:28 +0300

    Chore: add car builder files

---
+++ src/main/java/com/example/builder/Car.java
@@ -0,0 +1,32 @@
+public class Car {
+    private boolean hasGPS;
+    private boolean hasTripComputer;
+    private int numberOfSeats;
+    private String engine;
+
+    public void setHasGPS(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+    }
+
+    public void setHasTripComputer(boolean hasTripComputer) {
+        this.hasTripComputer = hasTripComputer;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "hasGPS=" + hasGPS +
+                ", hasTripComputer=" + hasTripComputer +
+                ", numberOfSeats=" + numberOfSeats +
+                ", engine=" + engine +
+                '}';
+    }
+}
